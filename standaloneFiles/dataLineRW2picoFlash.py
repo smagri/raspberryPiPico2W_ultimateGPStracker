e3863aadfacacee63a12fbe2045c2b6adb6b0f78
Simone Magri <simone.j.magri@gmail.com>
2025-09-24 13:57:32 +1000

    Project16: Write and Read multiple lines to/from a logfile on the pico flash memory.

--- standaloneFiles/dataLineRW2picoFlash.py
+++ standaloneFiles/dataLineRW2picoFlash.py
@@ -1,32 +1,37 @@
 # This code writes  three sensor readings as  a comma-separated string
-# to log.txt on  the Pico W, then  reads the file back  and splits the
+# to a logfile on the Pico W,  then reads the file back and splits the
 # string into  a list.   Purpose: Demonstrate  basic file  writing and
-# reading in MicroPython using log.txt.
+# reading in MicroPython using a logfile.
 
 # Writing comma-delimited data to a file. Example sensor temperature
 # readings from three sensors
 sensorData = "25.5,26.0,24.8" 
 
 # Get object/handle to the logfile, can be named anything,
-# fileRWpicoFlash, Open log.txt in write mode ('w').
-with open('log.txt', 'w') as fileRWpicoFlash:
+# fileRWpicoFlash, Open logfile in write mode ('w').
+with open('dataLineRWlog.txt', 'w') as fileRWpicoFlash:
     # Write the comma-separated string to the file
     fileRWpicoFlash.write(sensorData)
-print("Data written to log.txt")  # Confirm data was written
+# Confirm data was written    
+print("csv Sensor Data string written to dataLineRWlog.txt")
 
 
 
 # Reading comma-delimited data
 
-# NOTE: mycropython can  use single quotes or doulbe  quotes but don't
+# NOTE: mycropython can  use single quotes or double  quotes but don't
 # mix  them.  with  open,  takes  care of  closing  the  file  at  the
 # appropriate time so you don't have to close them.
 
-with open('log.txt', 'r') as fileRWpicoFlash:  # Open log.txt in read mode ('r')
-    content = fileRWpicoFlash.read()  # Read the entire file as a string
-    print("Raw data:", content)  # Print the raw string
-    # Parse into a list/array
+# Open logfile in read mode ('r')
+with open('dataLineRWlog.txt', 'r') as fileRWpicoFlash:
+    rawData = fileRWpicoFlash.read()  # Read the entire file as a string
+    print("Raw data read from dataLineRWlog.txt:", rawData)
+
+    # Lets do something with our raw data for demonstration.  Hence, parse
+    # into a list/array.
 
     # Split the string at commas to create a list/array of strings.
-    values = content.split(',')
-    print("Parsed data:", values)  # Print the resulting list
+    rawData2rayStrs = rawData.split(',')
+    # Print the resulting list
+    print("Parsed raw data into list/array of strings:", rawData2rayStrs)
